668b7c2e0e09db1e55f4f03e0e5f6e44776cc33c
Christian Schwarz <schwarz.christian.de@gmail.com>
2018-05-27 20:04:57 +0200

    added zoom limit for quest request

--- js/gui.js
+++ js/gui.js
@@ -18,7 +18,7 @@ function handleResize()
 	}
 	else
 	{
-		//landscape	
+		//landscape
 		div_map.style.height = "100%";
 		if(questWindowShown)
 		{
@@ -27,8 +27,8 @@ function handleResize()
 		else
 		{
 			div_map.style.width = "100%";
-		}		
-	}
+		}
+		}
 }
 window.addEventListener("resize", handleResize);
 handleResize();
@@ -36,7 +36,7 @@ handleResize();
 //quest gui test
 function onClick(e)
 {
-	questWindowShown = false;	
+	questWindowShown = false;
 	handleResize();
 }
 ll_map.on("click", onClick);
@@ -69,34 +69,44 @@ function showQuestDetails(quest)
 	handleResize();
 }
 
-function refreshQuests()
+function removeQuestMarkers()
 {
-	let bbox = ll_map.getBounds();
-	bbox = `${bbox.getSouth()},${bbox.getWest()},${bbox.getNorth()},${bbox.getEast()}`;
-	findQuests(bbox).then(function(quests)
+	ll_map.eachLayer(function(layer)
 	{
-		ll_map.eachLayer(function(layer)
+		if(layer.is_quest && layer.is_quest_marker == true)
 		{
-            if(layer.is_quest && layer.is_quest_marker == true)
-            {
-				ll_map.removeLayer(layer);
-            }
-		});
-
-		for(let quest of quests)
-		{
-			let layer = quest.render();
-			layer.addTo(ll_map);
-
-			let marker = L.marker(layer.getCenter(), {icon: quest.icon});
-            marker.is_quest_marker = true;
-			marker.addTo(ll_map);
-
-			let showQuest = showQuestDetails.bind(null, quest);
-			layer.on("click", showQuest);
-			marker.on("click", showQuest);
+			ll_map.removeLayer(layer);
 		}
 	});
+}
+var cnt = 0;
+function refreshQuests()
+{
+	if(ll_map.getZoom() > 12){
+		let bbox = ll_map.getBounds();
+		bbox = `${bbox.getSouth()},${bbox.getWest()},${bbox.getNorth()},${bbox.getEast()}`;
+		findQuests(bbox).then(function(quests)
+		{
+			removeQuestMarkers();
+			for(let quest of quests)
+			{
+				let layer = quest.render();
+				layer.addTo(ll_map);
+
+				let marker = L.marker(layer.getCenter(), {icon: quest.icon});
+				marker.is_quest_marker = true;
+				marker.addTo(ll_map);
+
+				let showQuest = showQuestDetails.bind(null, quest);
+				layer.on("click", showQuest);
+				marker.on("click", showQuest);
+			}
+		});
+	}
+	else
+	{
+		removeQuestMarkers();
+	}
 }
 ll_map.on("moveend", refreshQuests);
 refreshQuests();
@@ -105,33 +115,33 @@ var pos_marker;
 var pos_radius;
 function update_location(position)
 {
-   // var radius = pos.accuracy / 2;
-   var latlng = [position.coords.latitude,  position.coords.longitude];
-   var pos_marker_new = L.marker(latlng).addTo(ll_map);
-   var  pos_radius_new = L.circle(latlng, {
-        color: 'blue',
-        fillColor: '#4c70ff',
-        fillOpacity: 0.5,
-        radius: position.coords.accuracy
-    }).addTo(ll_map);
-    ll_map.setView(latlng, ll_map.getZoom());   
-    if(pos_marker){
-        pos_marker.remove();
-        pos_radius.remove();
-    }
-    pos_marker = pos_marker_new;
-    pos_radius = pos_radius_new;
+	// var radius = pos.accuracy / 2;
+	var latlng = [position.coords.latitude,  position.coords.longitude];
+	var pos_marker_new = L.marker(latlng).addTo(ll_map);
+	var  pos_radius_new = L.circle(latlng, {
+		color: 'blue',
+		fillColor: '#4c70ff',
+		fillOpacity: 0.5,
+		radius: position.coords.accuracy
+	}).addTo(ll_map);
+	ll_map.setView(latlng, ll_map.getZoom());
+	if(pos_marker){
+		pos_marker.remove();
+		pos_radius.remove();
+	}
+	pos_marker = pos_marker_new;
+	pos_radius = pos_radius_new;
 }
 watchId = navigator.geolocation.watchPosition(
-     update_location,
-     // Optional settings below
-     function()
-     {
-        alert("Failed to aquire location. Enable GPS or try a different browser");
-     },
-     {
-         timeout: 0,
-         enableHighAccuracy: true,
-         maximumAge: Infinity
-     }
+	update_location,
+	// Optional settings below
+	function()
+	{
+		alert("Failed to aquire location. Enable GPS or try a different browser");
+	},
+	{
+		timeout: 0,
+		enableHighAccuracy: true,
+		maximumAge: Infinity
+	}
 );
