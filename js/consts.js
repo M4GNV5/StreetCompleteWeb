33406d3c89d1a0528cdd15f3b1734f4b7d567781
Christian Schwarz <schwarz.christian.de@gmail.com>
2018-05-27 13:27:04 +0200

    minor style improvements

---
+++ js/consts.js
@@ -0,0 +1,2 @@
+var ll_map = L.map('div_map').setView([48.51, 11.48], 13);
+var div_map = document.getElementById('div_map');
--- js/gui.js
+++ js/gui.js
@@ -1,5 +1,3 @@
-var ll_map = L.map('div_map').setView([48.51, 11.48], 13);
-var div_map = document.getElementById('div_map');
 var questWindowShown = false;
 
 function handleResize()
